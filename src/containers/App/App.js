1f2e5f430e6c33b206c6d08144bf830b641d1071
Alaeddine Messadi <10847134+AlaeddineMessadi@users.noreply.github.com>
2018-10-26 14:24:52 +0200

    Add Aux a higher order components

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import Header from '../../components/Header/Header';
 import Bargraph from '../../components/Graph/Bargraph/Bargraph';
+import Aux from '../../hoc/Aux';
 import './App.module.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Aux>
         <Header />
         <main>
           <Bargraph />
         </main>
-      </div>
+      </Aux>
     );
   }
 }
