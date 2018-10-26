91202233efeef76caf7849fa0a366efa98b03b9f
Alaeddine Messadi <10847134+AlaeddineMessadi@users.noreply.github.com>
2018-10-26 14:20:35 +0200

    Define Bargraph component

---
+++ src/components/Graph/Bargraph/Bargraph.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import styles from './Bargraph.module.scss';
+
+export default class Bargraph extends Component {
+  render() {
+    return (
+      <div>
+        History Bar graph
+      </div>
+    )
+  }
+}
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,13 +1,16 @@
-import React, { Component } from "react";
-import Header from "../../components/Header/Header";
-import "./App.module.scss";
+import React, { Component } from 'react';
+import Header from '../../components/Header/Header';
+import Bargraph from '../../components/Graph/Bargraph/Bargraph';
+import './App.module.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <main>History Bar graph</main>
+        <main>
+          <Bargraph />
+        </main>
       </div>
     );
   }
