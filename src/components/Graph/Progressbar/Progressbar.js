5a3a1e740541675ab2f704d978cb874234c8a113
AlaeddineMessadi <10847134+AlaeddineMessadi@users.noreply.github.com>
2018-10-26 14:38:46 +0200

    Use destructuring prop assignment in Progressbar.js

--- src/components/Graph/Progressbar/Progressbar.js
+++ src/components/Graph/Progressbar/Progressbar.js
@@ -1,13 +1,10 @@
 import React from "react";
 import styles from "./Progressbar.module.scss";
 
-const Progressbar = props => {
-  console.log(props.height)
-  return (
-    <dd className={ styles.progressbar } style={ { padding: `0 ${props.margin}` } }>
-      <span style={ { height: `${props.percent}%` } } data-value={ props.value } />
-    </dd>
-  );
-};
+const Progressbar = ({ value, percent }) => (
+  <dd className={ styles.progressbar }>
+    <span style={ { height: `${percent}%` } } data-value={ value } />
+  </dd>
+);
 
 export default Progressbar;
