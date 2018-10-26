00fa20c315211c2f579a90b99680ab7cc0cf674b
Alaeddine Messadi <10847134+AlaeddineMessadi@users.noreply.github.com>
2018-10-26 17:02:26 +0200

    Swich to display flex + reverse the sessions from right to left + set a progressbar maxwidth

--- src/components/Graph/Bargraph/Bargraph.js
+++ src/components/Graph/Bargraph/Bargraph.js
@@ -5,10 +5,12 @@ import styles from "./Bargraph.module.scss";
 
 export default class Bargraph extends Component {
   render() {
+    // dummy data
     let res = [];
     for (let i = 0; i <= 12; i++) {
       res.push(
         <Progressbar
+          key={i}
           height={Math.floor(Math.random() * 100)}
           value={Math.floor(Math.random() * 9999)}
         />
